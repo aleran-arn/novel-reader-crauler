9ea3b8126f74f5604a0334770f9b076e5b96199d
aleran-arn <71211578+aleran-arn@users.noreply.github.com>
2021-06-10 20:25:05 +0300

    Update confs

--- config/conf.js
+++ config/conf.js
@@ -1,8 +1,4 @@
 module.exports = {
     novelreader : 'https://novelfull.com',
-    lastNovelReaderPath : '/index.php/latest-release-novel?page=%d',
-    azureStoregeUrl: 'DefaultEndpointsProtocol=https;AccountName=novelreaderstore;AccountKey=mQqlWRlPRXjAz8D1XpABmKLeW67zsGmSeW5yu3iVni1bF6z7uXaw72l1b7J4nNrxnIpcBY7YmrbXBBgC/JAxog==;EndpointSuffix=core.windows.net',
-    azureStorageAccessUrl: 'https://novelreaderstore.blob.core.windows.net',
-    azureStorageCoverContainer: 'covers',
-    mongoUrl: 'mongodb://novel-reader-db:ePIUUbzKcNU05fxxN3nAmh7vforvtLHgQlifVJuyJP79s8Fkg7p0ez9euv1CDSbnTUCIKMKxoFDhe7EBe06YEw%3D%3D@novel-reader-db.mongo.cosmos.azure.com:10255/?ssl=true&appName=@novel-reader-db@',
+    lastNovelReaderPath : '/index.php/latest-release-novel?page=%d'
   };
